36f07343f240ad3cf6b0c6e117c3c70a2d1c8a48
nar789 <admin@hume.co.kr>
2023-08-23 20:24:33 +0900

    suqrt

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { OrbitControls } from '@react-three/drei';
 import {Happy} from './Happy'
 import { Arm2 } from './Arm2';
 import { Neck } from './Neck';
+import { Squart } from './Squart';
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
   return (
     <div className='back'>
       <Canvas  shadows camera={{ position: [0, 0, 10], fov: 50 }}>
-        <Neck position={[0, -2, 0]}/>
+        <Squart position={[0, -2, 0]}/>
         <ambientLight intensity={0.8}/>
         <spotLight position={[0.5, 2, 0.5]} angle={0.5} penumbra={2} />
         <pointLight position={[0, 1, 2]} intensity={20} />
