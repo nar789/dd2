51630c72d023ea3f8a6d4485c9dd7d9fd66d41b8
nar789 <admin@hume.co.kr>
2023-08-23 21:39:42 +0900

    flag

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Arm2 } from './Arm2';
 import { Neck } from './Neck';
 import { Squart } from './Squart';
 import { Pushup } from './Pushup';
+import { Flag } from './Flag';
 
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
   return (
     <div className='back'>
       <Canvas  shadows camera={{ position: [0, 0, 10], fov: 50 }}>
-        <Pushup position={[0, 0, 0]} rotation={[0, -Math.PI / 4, -Math.PI / 8]} />
+        <Flag position={[0, -2, 0]} />
         <ambientLight intensity={0.8}/>
         <spotLight position={[0.5, 2, 0.5]} angle={0.5} penumbra={2} />
         <pointLight position={[0, 1, 2]} intensity={20} />
