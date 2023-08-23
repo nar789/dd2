fff0600d38b561fdd8b7662358aa5b1c9f7374ab
nar789 <admin@hume.co.kr>
2023-08-23 19:29:40 +0900

    push test woodmen

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import { Canvas } from '@react-three/fiber';
 import './App.css';
 import { OrbitControls } from '@react-three/drei';
 import {Happy} from './Happy'
+import { Arm } from './Arm';
+import { Arm2 } from './Arm2';
 
 
 function App() {
 
   return (
     <div className='back'>
-      <Canvas  shadows camera={{ position: [0, 0, 4], fov: 50 }}>
+      <Canvas  shadows camera={{ position: [0, 0, 10], fov: 50 }}>
+        <Arm2 position={[0, -2, 0]}/>
         <ambientLight intensity={0.8}/>
         <spotLight position={[0.5, 2, 0.5]} angle={0.5} penumbra={2} />
-        <pointLight position={[0, 1, 2]} intensity={3} />
+        <pointLight position={[0, 1, 2]} intensity={20} />
         <pointLight position={[1, 1, 1]} />
-        <Happy />
+        
         <OrbitControls />
         <axesHelper />
       </Canvas>
